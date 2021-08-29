ef709cdfe430bfd813564a3a6caf96ad55af1b45
CHRISTIAN <cespinoza@ueat.io>
2021-08-29 16:44:00 -0500

    first commit

---
+++ customer-service/src/main/java/com/test/customer/CustomerService.java
@@ -0,0 +1,13 @@
+package com.test.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CustomerService {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerService.class, args);
+    }
+}
